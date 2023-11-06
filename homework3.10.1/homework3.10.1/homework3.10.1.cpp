d26e7ca2eabc310ecd5396ce73f5937f8b95be23
MayoWarrior <sweetyl4255.55@gmail.com>
2023-11-06 21:07:04 +0300

    homework

---
+++ homework3.10.1/homework3.10.1/homework3.10.1.cpp
@@ -0,0 +1,17 @@
+﻿// homework3.10.1.cpp: определяет точку входа для приложения.
+//
+
+#include "homework3.10.1.h"
+
+
+
+int main()
+{
+	system("chcp 1251");
+	std::string name;
+	std::cout << "Введите имя: ";
+	std::cin >> name;
+	std::cout << "\nЗдравствуйте, " << name << std::endl;
+	system("pause");
+	return 0;
+}
